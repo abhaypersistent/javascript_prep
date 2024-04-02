f05a464379e7813245ea3f8b270a626525171f60
Abhay Pandey <abhay_pandey@persistent.com>
2024-04-02 12:55:40 +0530

    accordian

--- machineRound2/accordian/index.js
+++ machineRound2/accordian/index.js
@@ -2,26 +2,25 @@ const mainContent = document.querySelector('.accordian-container');
 const checkboxVal = document.getElementById('check-val')
 
 mainContent.addEventListener('click', function(e){
-    console.log(e.target.getAttribute('data-id'));
-    manageAccordian(e.target.getAttribute('data-id'));
-    
+    manageAccordian(e.target.getAttribute('data-id'));    
 })
 
 function manageAccordian(id){
     if(id && id.indexOf('step-') < 0) return;
     let stepID = id.split('step-');
     let targetElement = document.querySelector(`[data-id=data-content-${stepID[1]}]`);
-    if(!checkboxVal.checked) closeAllTab();
     if(targetElement.classList.contains('active')){
         targetElement.classList.remove('active');
     }  else{
         targetElement.classList.add('active');
+        if(!checkboxVal.checked) closeAllTab(targetElement);
     }
 }
 
-function closeAllTab(){
+function closeAllTab(targetElement){
     let accordianData = document.getElementsByClassName('accordian-content');
     for (const iterator of accordianData) {
-        iterator.style.setProperty('max-height','0')
+        if(iterator.getAttribute('data-id') !== targetElement.getAttribute('data-id'))
+            iterator.classList.remove('active');
     }
 }
